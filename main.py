b55df784ecda74a1167cf1f2cb49c6eaf43be792
Forshteven <geocinds@mail.ru>
2024-08-12 15:01:40 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#"1st program"
+print(9**0.5*5)
+
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+
+#"4thprogram"
+a='123.456'
+print(a[4])
+print(float('123.456')*10%1230.56)
